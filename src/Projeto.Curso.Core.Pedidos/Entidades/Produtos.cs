32c59064b7f7fe29c9bc5fe8f8b84ada3c7ec464
Pedro Bruno <pedrobrunor@hotmail.com>
2018-05-13 13:19:22 -0300

    Aulas 05-06-07

---
+++ src/Projeto.Curso.Core.Pedidos/Entidades/Produtos.cs
@@ -0,0 +1,18 @@
+﻿using Projeto.Curso.Core.Domain.Shared.Entidades;
+
+namespace Projeto.Curso.Core.Domain.Pedidos.Entidades
+{
+    public class Produtos : EntidadeBase
+    {
+        public string Apelido { get; set; }
+        public string Nome { get; set; }
+        public decimal Valor { get; set; }
+        public string Unidade { get; set; }
+        public int IdFornecedor { get; set; }
+
+        public override bool EstaConsistente()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
